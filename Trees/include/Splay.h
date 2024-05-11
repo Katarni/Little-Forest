8daee33563d0db25256df052db3d7e74ed194bc3
Тимур Ахметзянов <ahmetzianov.temur@ya.ru>
2024-05-11 13:35:09 +0300

    added splay node

--- Trees/include/Splay.h
+++ Trees/include/Splay.h
@@ -7,5 +7,43 @@
 #include "includes.h"
 
 class Splay {
+ public:
+
+  class node {
+   public:
+    node(int64_t key) : key_(key), left_(nullptr), right_(nullptr) {}
+
+    node*& getLeft() {
+      return left_;
+    }
+
+    node*& getRight() {
+      return right_;
+    }
+
+    void setLeft(node* left) {
+      left_ = left;
+    }
+
+    void setRight(node* right) {
+      right_ = right;
+    }
+
+    static void clear(node*& node) {
+      if (node == nullptr) return;
+      clear(node->left_);
+      clear(node->right_);
+      delete node;
+    }
+
+    int64_t getKey() const {
+      return key_;
+    }
+
+   private:
+    int64_t key_;
+    node *left_, *right_;
+  };
+
 
 };
